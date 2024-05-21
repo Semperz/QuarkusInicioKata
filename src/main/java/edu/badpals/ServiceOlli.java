e5783340f8d68f75b1aaa703a9f24d1ef8e43c61
SergioPrieto <serprigar@gmail.com>
2024-05-21 13:21:57 +0200

    feat(ServiceOlli): comandaMultiple task

--- src/main/java/edu/badpals/ServiceOlli.java
+++ src/main/java/edu/badpals/ServiceOlli.java
@@ -4,7 +4,10 @@ import edu.badpals.domain.Item;
 import edu.badpals.domain.Orden;
 import edu.badpals.domain.Usuaria;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +29,7 @@ public class ServiceOlli {
     public List<Orden> cargaOrden(String usuaria_nombre) {
         return Orden.findByUserName(usuaria_nombre);
     }
-
+    @Transactional
     public Orden comanda(String usuaria, String objeto) {
         Orden orden = null;
         Optional<Usuaria> usuario = Usuaria.findByIdOptional(usuaria);
@@ -40,7 +43,19 @@ public class ServiceOlli {
         return orden;
     }
 
-    public List<Orden> comandaMultiple(String usuaria, List<String> objeto) {
-        return null;
+    public List<Orden> comandaMultiple(String usuaria, List<String> objetos) {
+        Optional<Usuaria> usuario = Usuaria.findByIdOptional(usuaria);
+        if (usuario.isEmpty()){
+            return Collections.emptyList();
+        }
+        List<Orden> comandas = new ArrayList<>();
+        Orden orden = null;
+        for (String objeto: objetos){
+            orden = this.comanda(usuaria,objeto);
+            if (orden != null){
+                comandas.add(orden);
+            }
+        }
+        return comandas;
     }
 }
